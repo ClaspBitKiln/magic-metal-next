2d9fdddc7622931311449fc287f6010ea1f2843b
ClaspBitKiln <yurtaev74@gmail.com>
2026-08-28 17:49:05 +0300

    Add site QA automation: qa/config.ts

---
+++ qa/config.ts
@@ -0,0 +1,19 @@
+export const pages = [
+  '/',
+  '/produkciya/truby-elektrosvarnye',
+  '/produkciya/truby-besshovnye',
+  '/produkciya/listovoj-prokat',
+  '/spravochnik-gost',
+  '/spravochnik-materialov',
+  '/poisk?q=12%D0%A51%D0%9C%D0%A4',
+  '/kalkulyator-metalla',
+  '/postavki/uzbekistan',
+]
+
+export const viewports = [
+  { name: 'desktop', width: 1440, height: 1000 },
+  { name: 'tablet', width: 820, height: 1180 },
+  { name: 'mobile', width: 390, height: 844 },
+]
+
+export const forbiddenContent = ['Enter Engineering', 'UzGTL', 'космодром «Восточный»', 'АЛРОСА', 'Отправить спецификацию']
