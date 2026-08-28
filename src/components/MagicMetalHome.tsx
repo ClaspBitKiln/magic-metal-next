fd2a8e69b1a1f60c749e3d316728d5c26acd419b
ClaspBitKiln <yurtaev74@gmail.com>
2026-08-28 14:34:38 +0300

    Improve Rosatom experience card

--- src/components/MagicMetalHome.tsx
+++ src/components/MagicMetalHome.tsx
@@ -193,8 +193,9 @@ export default function MagicMetalHome() {
               <small>опыт поставок для подрядных организаций</small>
             </article>
             <article className="company-proof-card company-proof-card--atomic">
-              <strong>Атомная<br />промышленность</strong>
-              <small>подрядчики на строительных объектах</small>
+              <img src="https://commons.wikimedia.org/wiki/Special:Redirect/file/Rosatom_logo.png" alt="Госкорпорация «Росатом»" loading="lazy" />
+              <strong>Атомная промышленность России</strong>
+              <small>опыт работы с подрядными организациями на строительных объектах отрасли</small>
             </article>
             <article className="company-proof-card company-proof-card--roscosmos">
               <img src="https://commons.wikimedia.org/wiki/Special:Redirect/file/Roscosmos_logo_ru.svg" alt="Роскосмос" loading="lazy" />
